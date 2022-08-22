72dc30324b8ba8ea1d0f33b00fd6a0dd092361ad
Eleonora <norka2233@gmail.com>
2022-08-22 17:23:31 +0300

    creted views and urls for methods detail, result and vote

--- testsite/polls/views.py
+++ testsite/polls/views.py
@@ -3,3 +3,17 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World, This is Poll App.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
